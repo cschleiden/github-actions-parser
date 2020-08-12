641b2dedc4d36d459601da87c01b81fa77530909
Christopher Schleiden <cschleiden@github.com>
2020-08-11 20:25:47 -0700

    Fix matrix schema

--- src/lib/workflowschema/workflowSchema.ts
+++ src/lib/workflowschema/workflowSchema.ts
@@ -228,9 +228,9 @@ export function _getSchema(context: Context): NodeDesc {
               type: "map",
               keys: {
                 matrix: {
-                  type: "sequence",
+                  type: "map",
                   itemDesc: {
-                    type: "map",
+                    type: "sequence",
                   },
                   description:
                     "A build matrix is a set of different configurations of the virtual environment. For example you might run a job against more than one supported version of a language, operating system, or tool. Each configuration is a copy of the job that runs and reports a status.\nYou can specify a matrix by supplying an array for the configuration options. For example, if the GitHub virtual environment supports Node.js versions 6, 8, and 10 you could specify an array of those versions in the matrix.\nWhen you define a matrix of operating systems, you must set the required runs-on keyword to the operating system of the current job, rather than hard-coding the operating system name. To access the operating system name, you can use the matrix.os context parameter to set runs-on. For more information, see https://help.github.com/en/articles/contexts-and-expression-syntax-for-github-actions.",
